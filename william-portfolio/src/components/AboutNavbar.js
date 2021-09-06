5ce5492f7302720f0ba4a2d9970b3114f43b93c2
willewilsonson <william.nordenstad@gmail.com>
2021-09-06 12:40:44 +0200

    fixed css for about-nav

--- william-portfolio/src/components/AboutNavbar.js
+++ william-portfolio/src/components/AboutNavbar.js
@@ -12,18 +12,18 @@ const AboutNavbar = ({display, setDisplay}) => {
     return (
         <nav className="about__about-navbar">
         <ul className="about-navbar__content">
-            <li className="about-navbar__element">
-                <button className={`about-navbar__button${isActive('life')}`} id="life"
+            <li className={`about-navbar__element${isActive('life')}`}>
+                <button className='about-navbar__button' id="life"
                 onClick={() => setDisplay('life')}
                 >Life</button>
             </li>
-            <li className="about-navbar__element">
-                <button className={`about-navbar__button${isActive('education')}`} id="education"
+            <li className={`about-navbar__element${isActive('education')}`}>
+                <button className='about-navbar__button' id="education"
                 onClick={() => setDisplay('education')}
                 >Education</button>
             </li>
-            <li className="about-navbar__element">
-            <button className={`about-navbar__button${isActive('experiences')}`} id="experiences"
+            <li className={`about-navbar__element${isActive('experiences')}`}>
+                <button className='about-navbar__button' id="experiences"
                 onClick={() => setDisplay('experiences')}
                 >Experiences</button>
             </li>
