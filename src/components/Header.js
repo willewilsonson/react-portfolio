04d179304d145f85f65ae894f60bf91dbfc53cf2
willewilsonson <william.nordenstad@gmail.com>
2021-09-09 13:57:25 +0200

    fixed header text style

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,9 +10,11 @@ const Header = ({height}) => {
                 <img src={wnLogo} alt="" className="header__logo"></img>
                 <img src={profileImage} alt="" className="header__profile-image"></img>
             </div>
-            <h3 className="header__greeting">Hello</h3>
-            <h1 className="header__name">I'm William</h1>
-            <h3 className="header__description">A full stack JavaScript developer</h3>
+            <div className="header__text-wrapper">
+                <h3 className="header__greeting">Hello</h3>
+                <h1 className="header__name">I'm William</h1>
+                <h3 className="header__description">A full stack JavaScript developer</h3>
+            </div>
         </header>
     )
 }
