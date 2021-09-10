8ba63411d184189cc33de4fd819039aa824161a4
willewilsonson <william.nordenstad@gmail.com>
2021-09-10 16:27:27 +0200

    added gsap slide effect to projects

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,41 @@
+import React, { useEffect, useRef } from 'react';
+import gsap from 'gsap';
 import './Card.css';
 import todoImage from "../project_todo_image.png";
 import hackday from "../hackday.png";
 
-const Card = ({activeCard}) => {
+const Card = ({activeCard, direction}) => {
+    const slideCard = useRef();
+
+    const activeCardRef = (card) => {
+        if (activeCard !== card) {
+            return;
+        }
+        return slideCard;
+    };
+
+    const slideDirection = () => {
+        if (direction === true) {
+            return 500;
+        }
+        return -500;
+    }
+
+    useEffect(() => {
+        gsap.fromTo(slideCard.current, { duration: 2, x: slideDirection(), scale: .5, opacity: 0 }, { x: 0, scale: 1, opacity: 1 });
+    });
 
     return (
         <article className="card-wrapper">
             <div className={`card-wrapper__card${activeCard === 1 ? ' showCard' : ''}`}>
-                <img className="card__image" src={todoImage} alt="project_todo_image"></img>
+                <img ref={activeCardRef(1)} className="card__image" src={todoImage} alt="project_todo_image"></img>
                 <footer className="card__card-footer">
                     <a href="https://github.com/willewilsonson/todo" target="_blank" rel="noreferrer" className="card-footer__link">Code</a>
                     <a href="https://willewilsonson.github.io/todo/" target="_blank" rel="noreferrer" className="card-footer__link">Demo</a>
                 </footer>
             </div>
             <div className={`card-wrapper__card${activeCard === 2 ? ' showCard' : ''}`}>
-                <img className="card__image" src={hackday} alt="project_todo_image"></img>
+                <img ref={activeCardRef(2)} className="card__image" src={hackday} alt="project_todo_image"></img>
                 <footer className="card__card-footer">
                     <a href="https://github.com/willewilsonson/hackday" target="_blank" rel="noreferrer" className="card-footer__link">Code</a>
                 </footer>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,16 +2,28 @@ import React, { useState } from 'react';
 import './Projects.css';
 import Card from './Card';
 
+const cardsQuantity = 2;
+const counter = 1;
+
 
 const Projects = ({height}) => {
     const[activeCard, setActiveCard] = useState(1);
+    const[direction, setDirection] = useState(false);
 
     if (activeCard === 0) {
-        setActiveCard(2);
+        setActiveCard(cardsQuantity);
     }
-    if (activeCard  === 3) {
+    if (activeCard  > cardsQuantity) {
         setActiveCard(1);
     }
+
+    const handleClick = (direction) => {
+        setDirection(direction);
+        if (direction === false) {
+            setActiveCard(activeCard - 1);
+        }
+        setActiveCard(activeCard +1 );
+    };
 
     console.log('activecard ', activeCard);
 
@@ -21,12 +33,12 @@ const Projects = ({height}) => {
             <h1 className="projects__title">Projects</h1>
             <div className="projects__wrap">
                 <button className={"projects__button--left"}
-                    onClick={() => setActiveCard(activeCard - 1)}
+                    onClick={() => handleClick(true)}
                     >&#60;
                 </button>
-                <Card activeCard={activeCard} />
+                <Card activeCard={activeCard} direction={direction}/>
                 <button className={"projects__button--right"}
-                    onClick={() => setActiveCard(activeCard + 1)}
+                    onClick={() => handleClick(false)}
                     >&#62;
                 </button>
             </div>
